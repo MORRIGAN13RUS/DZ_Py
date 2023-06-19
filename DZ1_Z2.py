3e054a3cd69959531f3c3accd8ed816c8cfe9617
Morrigan <stepanyancarine@yandex.ru>
2023-06-19 22:51:13 +0300

    3

---
+++ DZ1_Z2.py
@@ -0,0 +1,10 @@
+# Нарисовать в консоли ёлку спросив
+# у пользователя количество рядов.
+number = int(input("Сколько рядов  елки? "))
+plus = 1
+space = number - 1
+for i in range(1, number+1):
+    print(' ' * space, end='')
+    space -= 1
+    print('+' * plus)
+    number +=2

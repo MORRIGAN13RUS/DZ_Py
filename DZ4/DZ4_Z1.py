536749c417a9b867094e58adfc257bedc5a5950c
Morrigan <stepanyancarine@yandex.ru>
2023-06-30 17:45:59 +0300

    2

--- DZ4/DZ4_Z1.py
+++ DZ4/DZ4_Z1.py
@@ -2,9 +2,12 @@
 # Напишите функцию для транспонирования матрицы
 
 
+
 def transposing_matrix(matrix):
     return list(zip(*matrix))
 
 
 if __name__ == '__main__':
-    print(transposing_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+    my_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    my_list_2 = (transposing_matrix(my_list))
+    print(*my_list,"\n",*my_list_2, sep="\n")
